e7fa789a1db6d7e8ecb3b70fdcda7356523b24e6
tanki13 <64957894+tanki13@users.noreply.github.com>
2020-05-29 21:58:10 +0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  display: inline;
+}
+</style>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+<div>
+<div class="w3-bar w3-black mainHeader">
+  <a href="#" class="w3-bar-item w3-button">Home</a>
+  <a href="#" class="w3-bar-item w3-button">Link 1</a>
+  <a href="#" class="w3-bar-item w3-button">Link 2</a>
+  <a href="#" class="w3-bar-item w3-button">Link 3</a>
+</div>
+</div>
+<p>
+As unveiled during the Six Invitational 2020, a major update is coming to the Tom Clancy’s Rainbow Six Siege’s esports programs. The four existing regions – Europe, North America, Latin America and Asia-Pacific – will now run in parallel their dedicated regional programs, all leading to the Six Invitational, the pinnacle of the Rainbow Six competitive year.
+
+For Europe, the action starts on June 22, with the first playday of the new European League.
+
+As we get ready for this next chapter of Rainbow Six Esports Europe, let’s review the key changes that you can expect.
+Global Structure
+
+Each regional league ties into a coherent global program, one unique race towards a qualification for the Six Invitational. As teams across the four regions face off in regional and global competitions, featuring 3 Majors and regional leagues, they will be ranked based on a new and in-depth point system that rewards sustained performance.
+
+Each competitive Season runs for nearly a year, from March through the following February. Each Season will be divided into 4 quarters. The first three quarters, corresponding to the 3 “Stages” of the regional leagues, consist of 5 weeks of play, and conclude with a Six Major bringing together 16 of the world’s best teams – four per region across the four regions. Each stage awards prize money and points for the Global Standings. The Global Standings determine the sixteen teams earning a spot to the annual Six Invitational. Following the 3 stages, the last quarter of the season will be dedicated to regional finals and relegations. The season will then conclude with the Six Invitational, the pinnacle of the Rainbow Six competitive year that will crown the world’s best team.
+
+Four additional teams will also have a chance to qualify for the Six Invitational through Regional Open Qualifiers, making the Six invitational a 20-team competition.
+
+A notable exception to the global structure will be the first Season of 2020. As the transition and sunset of the legacy Pro League program, this year will shorten the program to only 2 stages with two Six Majors this year.
+
+[R6S] News - EU Program
+The European League
+
+The European League represents the highest level of Rainbow Six: Siege competitive play in Europe. It will feature ten teams in total and for this first 2020 Season, the teams will be the eight teams from the European Pro League Season XI and the best two teams from the European Challenger League Season XI.
+
+Running in parallel with the other regional programs happening in Asia-Pacific, North America and Latin America, the European League will be broken down into 3 stages. Each stage consists of 9 playdays in a single round robin system, in which all teams play against one another once. A win is rewarded with 3 points, a draw 1 point and zero for a loss. Each stage ends once all the teams have faced each other once.
+
+The European League games will be broadcasted on Mondays and Wednesdays starting at 6pm CEST on the Rainbow Six Esports channels on Twitch and YouTube.
+
+Based on the European League points system, once a stage ends, the top 4 teams in the European League rankings will be qualified for the Six Major. They will also earn points for the Global Standings – in the race towards the Six Invitational – according to their final placements at the competition.
+
+Even if they will not get the chance to participate in the Six Major, the teams that rank between the 5th and 8th places will nonetheless score points for the Global Standings in the Six Invitational race.
+
+The 3 stages of the European League follow this same model. At the beginning of each new stage, the European League Rankings will be reset.
+
+Finally, at the end of all 3 stages of the European League, in November, the rankings of the 3 stages are combined to form the Final Rankings of the Season within the region. The top 4 teams will battle it out for the title of European Champion during a special event. On the other side of the Final Rankings, the last team will be relegated to the European Challenger League, while the team ranked 9th will compete in the Relegation Playoffs.
+
+[R6S] News - EU Program
+The European Challenger League and Nationals, foundation of the ecosystem
+
+The European Challenger League is the entry point to the European League. But before competing into the Challenger League, teams need to prove their skills on their National field.
+
+Our European scene draws its strength from its pool of highly skilled players from all over the region. Starting this Season, the European national programs will receive greater significance within the overall international program, thus forging a multi-tiered esports scene, allowing players from all levels and horizons to compete. Since the beginning of the Rainbow Six esports program 4 years ago, developing European local scenes has been a strong focus. With the new European program, we are going even further. A victory in the National program will be an entry point to the Challenger League as the National Champion will get a slot in the upcoming European Challenger League season. If the National Champions are already in the European League, then the runner-up takes the spot, and so on.
+
+At the end of the European Challenger League, the top 2 teams will have a chance to enter the next season of the European League.
+
+Those 2 teams will face each other during the Relegations Playoffs, organized during the Regional Finals. The winner of that match will take the place of the last ranking team of the European League. The loser will play an additional match against the team that ranked 9th in the European League to determine which of them joins the next season of European League.
+
+[R6S] News - EU Program
+
+To sum up: the European Challenger League will gather 12 teams divided as follows:
+
+    9 Champions from European National Championships. For the 2020 season, that includes France, GSA (Germany – Switzerland – Austria), the United Kingdom, Spain, Italy, the Nordics, Benelux, Poland and Russia.
+    2 teams coming from the Relegation system: the team that would be automatically relegated from the European League (the team ranking last) and the loser of the Relegation Playoffs. For our inaugural season in 2020 however, the 2 slots will be legacy slots based on the performance of the teams in the Challenger League Season XI. The slots will go the teams that ranked 3rd and 4th, respectively DefuseKids and PENTA.
+    1 Open Qualifier 
+
+Challenger league teams will engage in the following format:
+
+    Group phase: Teams will be divided into two groups of six teams based on seedings from previous seasons. It will consist of a Best of 1 single round robin. The four best teams of each group will advance to the playoffs.
+    Playoffs: The top 4 teams in each group will battle it out in November to determine the 2 teams that will reach the Regionals Finals. More details on the playoffs will be shared later on.
+    Regional Finals: see below. 
+
+The European Challenger League matches will start in the fall. More information will be shared later on.
+The road to become the European Champions
+
+The Regional Finals will happen in December to determine who the best team in the region this season truly is. In Europe, this will translate into a special event: the European League Finals (EU Finals).
+
+Based on the final rankings of the European League – aggregating the results of the 3 stages –, the top 4 teams of the European League will qualify for the EU Finals.
+
+This event will also host the Relegation Playoffs described above, to determine the 2 teams that will join the European League the following season. More information on the EU Finals format will be communicated later on.
+Discover a whole new broadcast experience
+
+With the 2020 Season, Ubisoft will take a first major step in revamping the viewing experience of its esports leagues and will inaugurate a brand-new and cutting-edge broadcast studio.
+
+While the European League and Challenger League matches will be happening online for our pro teams, our crew of talent will join us, in Paris, to cast from this new studio.
+Our cast of talent and everyone from Ubisoft working behind the scenes is excited to welcome you in the new home of Rainbow Six Esports Europe.
+
+Though we really want to keep the element of surprise on the look and feel of the studio for the moment you will tune in for the first match, in the meantime, here’s a sneak peek of the additions you can expect:
+
+• Premium tools and material to assist our talent while they cast.
+
+• A dedicated analyst area that will allow the crew to provide in-depth information.
+
+• New types of content on top of the action-packed show the teams will put on during their matches, such as debates, editorial pieces, interactive content and more.
+
+• Tech improvements backstage that will ultimately create a more engaging stream for the viewers to watch.
+
+As the health and well-being of our talent, staff and partners is our first priority, the new studio will not be ready for the first playdays of the European League. When the first matches begin on June 22, you will see a “light” version of the studio. This will be a temporary solution and as soon as the situation permits, the fully-fledged studio will take over! We will share more information soon. Stay tuned.
+
+To stay in the loop on everything related to our esports activities, follow Rainbow Six Esports as well as our Europe, North America, Asia-Pacific and Latin America regional accounts on Twitter. For behind-the-scenes coverage, follow Rainbow Six Esports on Instagram. And for much more on Rainbow Six Siege, check out our previous coverage. 
+<script src="https://apps.elfsight.com/p/platform.js" defer></script>
+<div class="elfsight-app-1cfb45ee-c3bf-4544-a626-4ef3d07435ca"></div>
+  
+
+
+</p>
+</body>
+</html>
+
